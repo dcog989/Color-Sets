bb381d268f13a4611727dc8cf6d874b1d0916ef5
dcog989 <sirdavidlaws@gmail.com>
2026-06-30 22:22:01 +0100

    practical decimal places

--- src/lib/formatColor.ts
+++ src/lib/formatColor.ts
@@ -5,9 +5,11 @@ export function formatColor(instance: Colordx, format: string): string {
     case 'rgb':
       return instance.toRgbString();
     case 'hsl':
-      return instance.toHslString();
-    case 'oklch':
-      return instance.toOklchString();
+      return instance.toHslString(0);
+    case 'oklch': {
+      const { l, c, h } = instance.toOklch(3);
+      return `oklch(${l} ${c} ${Math.round(h)})`;
+    }
     default:
       return instance.toHex();
   }
