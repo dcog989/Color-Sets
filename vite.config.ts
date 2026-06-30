1058f28a310e64bb723757340431614728b8c88c
dcog989 <sirdavidlaws@gmail.com>
2026-06-30 20:37:31 +0100

    remove firefox hack

--- vite.config.ts
+++ vite.config.ts
@@ -8,10 +8,4 @@ export default defineConfig({
   build: {
     sourcemap: false,
   },
-  server: {
-    host: '127.0.0.1', // Forces IPv4 to make Firefox behave, not wait 2 seconds for IPv6
-  },
-  preview: {
-    host: '127.0.0.1', // Forces IPv4 to make Firefox behave, not wait 2 seconds for IPv6
-  },
 });
